91111a7ab0b1984fb9deae1aafa567cc424ce3d9
JoshVanL <jv15626@my.bristol.ac.uk>
2018-05-03 02:50:39 +0100

    Unable to get the thing working

--- fault/attack.go
+++ fault/attack.go
@@ -135,6 +135,7 @@ func (a *Attack) Run() os.Error {
 	fmt.Printf("Attack Complete.\n")
 	fmt.Printf("Elapsed time: %.2fs\n*********\n", float((time.Nanoseconds()-now))/1e9)
 	fmt.Printf("Target material: [%X]\n", d)
+	fmt.Printf("Target material: [%v]\n", d)
 	fmt.Printf("Interactions: %d\n++++++++++++++++++++++++\n", a.interactions)
 
 	now = time.Nanoseconds()
@@ -182,35 +183,153 @@ func (a *Attack) SinglFaultAttack(c []byte) ([]byte, os.Error) {
 	fmt.Printf("Possible Keys: [%.0f]\n", total)
 	fmt.Printf("Progress: %.3f%", checked/total)
 
-	key := make([]byte, WORD_LENGTH)
-	for _, k1 := range hs[0] {
-		for _, k2 := range hs[1] {
-			for _, k3 := range hs[2] {
-				for _, k4 := range hs[3] {
+	reduced := a.ReduceHypotheses(hs, c, c2)
 
-					key = []byte{k1[0], k2[0], k3[0], k4[0],
-						k2[1], k3[1], k4[1], k1[1],
-						k3[2], k4[2], k1[2], k2[2],
-						k4[3], k1[3], k2[3], k3[3],
-					}
+	fmt.Printf("\n>>%d\n", len(reduced))
 
-					b, err := a.CheckKey(key)
-					if err != nil {
-						return nil, err
-					}
+	//key := make([]byte, WORD_LENGTH)
+	//for _, k1 := range hs[0] {
+	//	for _, k2 := range hs[1] {
+	//		for _, k3 := range hs[2] {
+	//			for _, k4 := range hs[3] {
 
-					if b {
-						return key, nil
-					}
+	//				key = []byte{k1[0], k2[0], k3[0], k4[0],
+	//					k2[1], k3[1], k4[1], k1[1],
+	//					k3[2], k4[2], k1[2], k2[2],
+	//					k4[3], k1[3], k2[3], k3[3],
+	//				}
 
-					checked++
-					fmt.Printf("\rProgress: %.3f%", checked/total)
-				}
-			}
+	//				b, err := a.CheckKey(key)
+	//				if err != nil {
+	//					return nil, err
+	//				}
+
+	//				if b {
+	//					return key, nil
+	//				}
+
+	//				checked++
+	//				fmt.Printf("\rProgress: %.3f%", checked/total)
+	//			}
+	//		}
+	//	}
+	//}
+
+	return nil, nil
+}
+
+func (a *Attack) ReduceHypotheses(hs [][][]byte, boo, foo []byte) [][]byte {
+	var reduced [][]byte
+
+	c := make([]byte, 17)
+	c[0] = 0
+	for i := 0; i < 16; i++ {
+		c[i+1] = foo[i]
+	}
+
+	c2 := make([]byte, 17)
+	c2[0] = 0
+	for i := 0; i < 16; i++ {
+		c2[i+1] = c2[i]
+	}
+
+	//i := a.conf.SBoxInv()[a.table[7][a.conf.SBoxInv()[c[1]^k[1]]^k[1]^a.conf.SBox()[k[14]^k[10]]^a.conf.RoundConstant()[10]]^
+	//	a.table[5][a.conf.SBoxInv()[c[14]^k[14]]^k[2]^a.conf.SBox()[k[15]^k[11]]]^
+	//	a.table[6][a.conf.SBoxInv()[c[11]^k[11]]^k[3]^a.conf.SBox()[k[16]^k[12]]]^
+	//	a.table[4][a.conf.SBoxInv()[c[8]^k[8]]^k[4]^a.conf.SBox()[k[13]^k[9]]]] ^ a.conf.SBoxInv()[a.table[7][a.conf.SBoxInv()[c2[1]^k[1]]^k[1]^a.conf.SBox()[k[14]^k[10]]^a.conf.RoundConstant()[10]]^
+	//	a.table[5][a.conf.SBoxInv()[c2[14]^k[14]]^k[2]^a.conf.SBox()[k[15]^k[11]]]^
+	//	a.table[6][a.conf.SBoxInv()[c2[11]^k[11]]^k[3]^a.conf.SBox()[k[16]^k[12]]]^
+	//	a.table[4][a.conf.SBoxInv()[c2[8]^k[8]]^k[4]^a.conf.SBox()[k[13]^k[9]]]]
+	//i := a.conf.SBoxInv()[a.table[14][a.conf.SBoxInv()[c[1]^k[1]]^k[1]^a.conf.SBox()[k[14]^k[10]]^a.conf.RoundConstant()[10]]^
+	//	a.table[11][a.conf.SBoxInv()[c[14]^k[14]]^k[2]^a.conf.SBox()[k[15]^k[11]]]^
+	//	a.table[13][a.conf.SBoxInv()[c[11]^k[11]]^k[3]^a.conf.SBox()[k[16]^k[12]]]^
+	//	a.table[9][a.conf.SBoxInv()[c[8]^k[8]]^k[4]^a.conf.SBox()[k[13]^k[9]]]] ^ a.conf.SBoxInv()[a.table[14][a.conf.SBoxInv()[c2[1]^k[1]]^k[1]^a.conf.SBox()[k[14]^k[10]]^a.conf.RoundConstant()[10]]^
+	//	a.table[11][a.conf.SBoxInv()[c2[14]^k[14]]^k[2]^a.conf.SBox()[k[15]^k[11]]]^
+	//	a.table[13][a.conf.SBoxInv()[c2[11]^k[11]]^k[3]^a.conf.SBox()[k[16]^k[12]]]^
+	//	a.table[9][a.conf.SBoxInv()[c2[8]^k[8]]^k[4]^a.conf.SBox()[k[13]^k[9]]]]
+
+
+	//j := a.conf.SBoxInv()[a.table[9][a.conf.SBoxInv()[c[13]^k[13]]^k[13]^k[9]]^
+	//	a.table[14][a.conf.SBoxInv()[c[10]^k[10]]^k[10]^k[14]]^
+	//	a.table[11][a.conf.SBoxInv()[c[7]^k[7]]^k[15]^k[11]]^
+	//	a.table[13][a.conf.SBoxInv()[c[4]^k[4]]^k[16]^k[12]]] ^ a.conf.SBoxInv()[a.table[9][a.conf.SBoxInv()[c2[13]^k[13]]^k[13]^k[9]]^
+	//	a.table[14][a.conf.SBoxInv()[c2[10]^k[10]]^k[10]^k[14]]^
+	//	a.table[11][a.conf.SBoxInv()[c2[7]^k[7]]^k[15]^k[11]]^
+	//	a.table[13][a.conf.SBoxInv()[c2[4]^k[4]]^k[16]^k[12]]]
+
+	//j := a.conf.SBoxInv()[a.table[4][a.conf.SBoxInv()[c[13]^k[13]]^k[13]^k[9]]^
+	//	a.table[7][a.conf.SBoxInv()[c[10]^k[10]]^k[10]^k[14]]^
+	//	a.table[5][a.conf.SBoxInv()[c[7]^k[7]]^k[15]^k[11]]^
+	//	a.table[6][a.conf.SBoxInv()[c[4]^k[4]]^k[16]^k[12]]] ^ a.conf.SBoxInv()[a.table[4][a.conf.SBoxInv()[c2[13]^k[13]]^k[13]^k[9]]^
+	//	a.table[7][a.conf.SBoxInv()[c2[10]^k[10]]^k[10]^k[14]]^
+	//	a.table[5][a.conf.SBoxInv()[c2[7]^k[7]]^k[15]^k[11]]^
+	//	a.table[6][a.conf.SBoxInv()[c2[4]^k[4]]^k[16]^k[12]]]
+	for _ = range hs {
+
+		//k := []byte{
+		//	0, h[0][0], h[1][0], h[2][0], h[3][0],
+		//	h[1][1], h[2][1], h[3][1], h[0][1],
+		//	h[2][2], h[3][2], h[0][2], h[1][2],
+		//	h[3][3], h[0][3], h[1][3], h[2][3],
+		//}
+		//0, 142, 174, 4, 40, 73, 157, 32, 231, 203, 66, 9, 103, 76, 219, 48, 31,
+		//0, 31, 48, 219, 76, 103, 9, 66, 203, 231, 32, 157, 73, 40, 4, 174, 142,
+		k := []byte{
+			0, 161, 51, 66, 184, 212, 196, 48, 106, 170, 33, 6, 121, 167, 232, 240, 235,
+			//0, 235, 240, 232, 167, 121, 6, 33, 170, 106, 48, 196, 212, 184, 66, 51, 161,
+		}
+		i := a.conf.SBoxInv()[a.table[7][a.conf.SBoxInv()[c[1]^k[1]]^k[1]^a.conf.SBox()[k[14]^k[10]]^a.conf.RoundConstant()[10]]^
+			a.table[5][a.conf.SBoxInv()[c[14]^k[14]]^k[2]^a.conf.SBox()[k[15]^k[11]]]^
+			a.table[6][a.conf.SBoxInv()[c[11]^k[11]]^k[3]^a.conf.SBox()[k[16]^k[12]]]^
+			a.table[4][a.conf.SBoxInv()[c[8]^k[8]]^k[4]^a.conf.SBox()[k[13]^k[9]]]] ^ a.conf.SBoxInv()[a.table[7][a.conf.SBoxInv()[c2[1]^k[1]]^k[1]^a.conf.SBox()[k[14]^k[10]]^a.conf.RoundConstant()[10]]^
+			a.table[5][a.conf.SBoxInv()[c2[14]^k[14]]^k[2]^a.conf.SBox()[k[15]^k[11]]]^
+			a.table[6][a.conf.SBoxInv()[c2[11]^k[11]]^k[3]^a.conf.SBox()[k[16]^k[12]]]^
+			a.table[4][a.conf.SBoxInv()[c2[8]^k[8]]^k[4]^a.conf.SBox()[k[13]^k[9]]]]
+		j := a.conf.SBoxInv()[a.table[4][a.conf.SBoxInv()[c[13]^k[13]]^k[13]^k[9]]^
+			a.table[7][a.conf.SBoxInv()[c[10]^k[10]]^k[10]^k[14]]^
+			a.table[5][a.conf.SBoxInv()[c[7]^k[7]]^k[15]^k[11]]^
+			a.table[6][a.conf.SBoxInv()[c[4]^k[4]]^k[16]^k[12]]] ^ a.conf.SBoxInv()[a.table[4][a.conf.SBoxInv()[c2[13]^k[13]]^k[13]^k[9]]^
+			a.table[7][a.conf.SBoxInv()[c2[10]^k[10]]^k[10]^k[14]]^
+			a.table[5][a.conf.SBoxInv()[c2[7]^k[7]]^k[15]^k[11]]^
+			a.table[6][a.conf.SBoxInv()[c2[4]^k[4]]^k[16]^k[12]]]
+
+		fmt.Printf("%d %d \n", i, j)
+		if i != a.table[2][j] {
+			fmt.Printf("HERE\n")
+			continue
+		}
+		fmt.Printf("HERE2!!!!!!!!!!!!!!!!!!!!!1\n")
+
+		m := a.conf.SBoxInv()[a.table[5][a.conf.SBoxInv()[c[9]^k[9]]^k[9]^k[5]]^
+			a.table[3][a.conf.SBoxInv()[c[6]^k[6]]^k[10]^k[6]]^
+			a.table[6][a.conf.SBoxInv()[c[3]^k[3]]^k[11]^k[7]]^
+			a.table[4][a.conf.SBoxInv()[c[16]^k[16]]^k[12]^k[8]]] ^ a.conf.SBoxInv()[a.table[5][a.conf.SBoxInv()[c2[9]^k[9]]^k[9]^k[5]]^
+			a.table[3][a.conf.SBoxInv()[c2[6]^k[6]]^k[10]^k[6]]^
+			a.table[6][a.conf.SBoxInv()[c2[3]^k[3]]^k[11]^k[7]]^
+			a.table[4][a.conf.SBoxInv()[c2[16]^k[16]]^k[12]^k[8]]]
+
+		if j != m {
+			continue
+		}
+
+		n := a.conf.SBoxInv()[a.table[4][a.conf.SBoxInv()[c[5]^k[5]]^k[5]^k[1]]^
+			a.table[5][a.conf.SBoxInv()[c[2]^k[2]]^k[6]^k[2]]^
+			a.table[3][a.conf.SBoxInv()[c[15]^k[15]]^k[7]^k[3]]^
+			a.table[6][a.conf.SBoxInv()[c[12]^k[12]]^k[8]^k[4]]] ^ a.conf.SBoxInv()[a.table[4][a.conf.SBoxInv()[c2[5]^k[5]]^k[5]^k[1]]^
+			a.table[5][a.conf.SBoxInv()[c2[2]^k[2]]^k[6]^k[2]]^
+			a.table[3][a.conf.SBoxInv()[c2[15]^k[15]]^k[7]^k[3]]^
+			a.table[6][a.conf.SBoxInv()[c2[12]^k[12]]^k[8]^k[4]]]
+
+		if a.table[1][m] != n {
+			continue
+		}
+
+		if a.table[1][i] == a.table[2][j] && a.table[2][j] == a.table[2][m] && a.table[2][m] == a.table[0][n] {
+			reduced = utils.AppendByte2(reduced, k)
 		}
 	}
 
-	return nil, nil
+	return nil
 }
 
 func (a *Attack) MultiFaultAttack(c []byte) ([]byte, os.Error) {
@@ -275,6 +394,7 @@ func (a *Attack) gatherHypotheses(c, c2 []byte, factor, index []int) [][]byte {
 }
 
 func (a *Attack) InverseKey(ik []byte) []byte {
+	fmt.Printf("%v\n", ik)
 	for i := 10; i > 0; i-- {
 		tmp := make([]byte, 12)
 		for j := 0; j < 12; j++ {
@@ -372,9 +492,9 @@ func (a *Attack) sameBytes(k1 []byte, k2 []byte) bool {
 }
 
 func BuildTable() [][]byte {
-	table := make([][]byte, 4)
+	table := make([][]byte, 16)
 
-	for i := 1; i < 4; i++ {
+	for i := 0; i < 16; i++ {
 
 		table[i] = make([]byte, KEY_RANGE)
 		for j := 0; j < KEY_RANGE; j++ {
